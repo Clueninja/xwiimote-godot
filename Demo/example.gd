ad2ab3d8c8c5ad40a44ecd332c4ffcb5f480e015
Clueninja <71099675+Clueninja@users.noreply.github.com>
2021-07-16 16:42:43 +0100

    Update example.gd

--- Demo/example.gd
+++ Demo/example.gd
@@ -1,11 +1,10 @@
 extends KinematicBody
 
 
-var player1 = load("res://bin/libxwiimote-godot.gdns").new()
+var player1 = load("res://libxwiimote-godot.gdns").new()
 var curacc =Vector3()
 var preacc = Vector3()
-#var curpos = Vector3()
-#var prepos =Vector3()
+
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
@@ -14,18 +13,10 @@ func _ready():
 	player1.open_iface()
 	player1.poll_iface()
 	preacc = Vector3(player1.accel_X(),  player1.accel_Z(), -player1.accel_Y())
-	#prerot = currot
-	#curpos =Vector3(-player1.motionplus_X(), player1.motionplus_Y(), player1.motionplus_Z())
-	#prepos = curpos
-
-
-	#input.bind_device_event("motionplus", "29")
-	#input.open_device("motionplus")
-
-
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(_delta):
 	player1.poll_iface()
 	

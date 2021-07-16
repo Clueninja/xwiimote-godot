048083eba5f8dbb3b612079a83515aea1138571e
Clueninja <71099675+Clueninja@users.noreply.github.com>
2021-07-16 21:06:27 +0100

    Update example.gd
    don't ask why pls

--- Demo/example.gd
+++ Demo/example.gd
@@ -1,27 +1,16 @@
-extends KinematicBody
 
-
-var player1 = load("res://libxwiimote-godot.gdns").new()
-var curacc =Vector3()
-var preacc = Vector3()
-
-# Called when the node enters the scene tree for the first time.
+var player1 = load("res://bin/libxwiimote-godot.gdns").new()
+var vel = Vector3(0,0,0)
 
 func _ready():
-
 	player1.set_iface(1)
 	player1.open_iface()
-	player1.poll_iface()
-	preacc = Vector3(player1.accel_X(),  player1.accel_Z(), -player1.accel_Y())
-	
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-
 func _process(_delta):
 	player1.poll_iface()
+	vel+= Vector3(player1.accel_X(),  player1.accel_Z()-95, -player1.accel_Y())*_delta*0.5
 	
-	curacc = Vector3(player1.accel_X(),  player1.accel_Z(), -player1.accel_Y())
-	global_translate((curacc-preacc)*_delta*0.1) # only works if wiimote is flat, if tilted it will give bad results
+	global_translate(vel*_delta*0.1) # only works if wiimote is flat, if tilted it will give bad results lol
 	
-	preacc = curacc
-
